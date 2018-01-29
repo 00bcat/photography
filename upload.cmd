3972bbc7195febb0811b354183e9591b525ff7fb
Johnson <j3.soon@msa.hinet.net>
2018-01-29 22:37:59 +0800

    Fix gulp bug on Windows

---
+++ upload.cmd
@@ -0,0 +1,12 @@
+:: Execute this after adding new photos in ./images/
+:: npm, gulp, ImageMagick
+:: Should install above beforehand
+
+set /p msg="Enter git commit message: "
+:: https://github.com/gulpjs/gulp/issues/1086
+call gulp
+git fetch
+git add --all
+git commit -m "%msg%"
+git push origin master
+pause
